4527fc2dc7b2867cd90141c5ef4ab1e7baed5e64
Gogginja <89789694+Gogginja@users.noreply.github.com>
2024-03-01 14:26:54 -0500

    Finished

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -10,15 +10,23 @@ function generateProductHTML(product: Product): string {
 }
 
 function renderProducts(prods: Product[]): void {
-    // your code
+    const mainContainer = document.getElementById('main-container');
+    if (!mainContainer) return;
+
+    mainContainer.innerHTML = '';
+
+    prods.forEach(product => {
+        const productHTML = generateProductHTML(product);
+        mainContainer.innerHTML += productHTML;
+    });
 }
 
-function getByCategory(category: string): void {
-    // your code
+function getByCategory(category: string): Product[] {
+    return products.filter(product => product.category === category);
 }
 
-function getByRating(minRating: number): void {
-    // your code
+function getByRating(minRating: number): Product[] {
+    return products.filter(product => product.rating > minRating);
 }
 
 export { renderProducts, getByCategory, getByRating };
